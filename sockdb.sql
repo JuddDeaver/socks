364c8e26af6c2fddcadeae1d94788c31c3a193bf
Judd Deaver <judddeaver@gmail.com>
2015-07-21 10:02:13 -0700

    add create schema

--- sockdb.sql
+++ sockdb.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `socks` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `socks`;
 -- MySQL dump 10.13  Distrib 5.6.22, for osx10.8 (x86_64)
 --
 -- Host: 127.0.0.1    Database: socks
@@ -310,4 +312,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2015-07-21  9:54:18
+-- Dump completed on 2015-07-21 10:01:50
